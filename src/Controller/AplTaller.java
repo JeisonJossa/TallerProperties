ddf33e3b9c93e89fbe24dd3e04c8d6047f0d870c
JeisonJossa <jeisonjossa77@gmail.com>
2022-12-21 23:01:20 -0500

    Primer commit

---
+++ src/Controller/AplTaller.java
@@ -0,0 +1,13 @@
+package Controller;
+
+
+
+public class AplTaller {
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Controlador aja = new Controlador();
+		
+	}
+	
+}
